13d8352a39ba0e12bdb9f799df6c2d42b99ea809
Muhammad-Kareem123 <muhammad.0523027@gmail.com>
2024-11-18 02:43:51 +0200

    new_commit

---
+++ Data_Grid/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Data_Grid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            McDataGrid.ItemsSource = LoadCollectionData();
+        }
+
+        private List<Author> LoadCollectionData()
+        {
+            List<Author> authors = new List<Author>();
+            authors.Add(new Author()
+            {
+                ID = 101,
+                Name = "Mahesh Chand",
+                BookTitle = "Graphics Programming with GDI+",
+                DOB = new DateTime(2007,6,4),
+                IsMVP = false,
+            });
+            authors.Add(new Author()
+            {
+                ID = 201,
+                Name = "Mike Gold",
+                BookTitle = "Programming C#",
+                DOB= new DateTime(2007,6,4),
+                IsMVP = true,
+            });
+            return authors;
+        }
+    }
+    public class Author
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string BookTitle { get; set;}
+        public bool IsMVP { get; set; }
+    }
+}
